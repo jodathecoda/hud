ecc781d2a55a2a27c3865bf88f8a46264c0f26e5
Joda TheCoda <jodathecoda@yahoo.com>
2022-01-06 14:40:15 +0200

    going there

--- hud.py
+++ hud.py
@@ -10,11 +10,10 @@ global cwd
 cwd = os.getcwd()
 
 # Adjust size
-root.geometry( "200x350" )
+root.geometry( "300x300" )
 
 def sb_open():
-    pass
-    #sb_open_button.config(text="Open 20%")
+    lab_sb_open_button.config(text="20%")
 
 selected = ""
 
@@ -54,30 +53,41 @@ clicked.set( "Monday" )
 
 # Create Dropdown menu
 drop = OptionMenu( root , clicked , *options )
-drop.pack()
+drop.grid(column=0, row=0)
+#drop.pack()
 
 # Create button, it will change label text
-button = Button( root , text = "Select" , command = show ).pack()
+button = Button( root , text = "Select" , command = show )#.pack()
+button.grid(column=1, row=0)
 
 # Create Label
 label1 = Label( root , text = " " )
-label1.pack()
+label1.grid(column=2, row=0)
 
 label2 = Label( root , text = " ----- PREFLOP SB -----" )
-label2.pack()
-sb_open_button = Button( root , text = "Open" , command = sb_open ).pack()
-sb_fold_button = Button( root , text = "Fold" , command = show ).pack()
+label2.grid(column=0, row=1)
+sb_open_button = Button( root , text = "Open" , command = sb_open )#.pack()
+sb_open_button.grid(column=0, row=2)
+lab_sb_open_button = Label( root , text = " 0%" )
+lab_sb_open_button.grid(column=1, row=2)
+sb_fold_button = Button( root , text = "Fold" , command = show )#.pack()
+sb_fold_button.grid(column=0, row=3)
 
 label3 = Label( root , text = " ----- PREFLOP BB -----" )
-label3.pack()
-bb_fold_button = Button( root , text = "Fold" , command = show ).pack()
-bb_call_button = Button( root , text = "Call" , command = show ).pack()
-bb_raise_button = Button( root , text = "Raise" , command = show ).pack()
+label3.grid(column=0, row=4)
+bb_fold_button = Button( root , text = "Fold" , command = show )#.pack()
+bb_fold_button.grid(column=0, row=5)
+bb_call_button = Button( root , text = "Call" , command = show )#.pack()
+bb_call_button.grid(column=0, row=6)
+bb_raise_button = Button( root , text = "Raise" , command = show )#.pack()
+bb_raise_button.grid(column=0, row=7)
 
 label4 = Label( root , text = " ----- POSTFLOP -----" )
-label4.pack()
-sb_cbet_button = Button( root , text = "Cbet" , command = show ).pack()
-sb_check_button = Button( root , text = "Check" , command = show ).pack()
+label4.grid(column=0, row=8)
+sb_cbet_button = Button( root , text = "Cbet" , command = show )#.pack()
+sb_cbet_button.grid(column=0, row=9)
+sb_check_button = Button( root , text = "Check" , command = show )#.pack()
+sb_check_button.grid(column=0, row=10)
 
 # Execute tkinter
 root.mainloop()
